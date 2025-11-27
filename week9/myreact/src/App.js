a8ce85b56fc09d8cfd2b5d7f51c7d41d66cd4764
sujanchand600-cell <sujanchand600@gmail.com>
2025-11-27 17:42:33 +0000

    week9

--- week9/myreact/src/App.js
+++ week9/myreact/src/App.js
@@ -21,5 +21,5 @@ function App() {
     </div>
   );
 }
-
+console.log
 export default App;
--- week9/myreact/src/index.js
+++ week9/myreact/src/index.js
@@ -10,7 +10,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <FacebookEmojiCounter type='Love' />
-    <FacebookEmojiCounter type='like' />
+    <FacebookEmojiCounter type='Like' />
     <FacebookEmojiCounter type='happy' />
   </React.StrictMode>
 );
